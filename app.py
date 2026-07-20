f344fe357662d11e245ef6793cf39f0b19c36675
Doris Wei <doris.xy.wei@gmail.com>
2026-07-20 21:39:52 +1000

    played around w requests

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-# initial setup
-# playing around
+import requests, os
+from dotenv import load_dotenv
+load_dotenv()
+
+url = "https://api.themoviedb.org/3/search/movie"
+
+params = {
+    "query": "huh",
+    "include_adult": False,
+    "language": "en-US",
+    "page": 2
+}
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {os.getenv('TMDB_API_KEY')}"
+}
+
+response = requests.get(url, params=params, headers=headers)
+print(response.json())
